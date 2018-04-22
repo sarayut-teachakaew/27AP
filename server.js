a3402a8d658b9adafad4233448c05209325a8dd6
saraT <sarayut_te@cmu.ac.th>
2018-04-22 08:29:50 +0700

    เพิ่มการยิง กระสุน เลือด การตาย คะแนน อัพเกรดต่างๆ

--- server.js
+++ server.js
@@ -5,40 +5,50 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/static'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendfile('index.html');
 });
 
 var idBase = 0;
 var players = [];
-io.on('connection', function(socket){
-    var id=0;
-    idBase++;
-    socket.emit('user id',idBase);
+io.on('connection', function (socket) {
+  var id = 0;
+  idBase++;
+  socket.emit('user id', idBase);
 
-    socket.on('player connect', function(play){
-      //console.log("sending");
-      //console.log(players);
-      socket.emit('load game',{player:players});
-      id=play.id;
-      players.push(play);
-      io.emit('player connect', play);
-      console.log("Total Player :"+players.length);
-    });
+  socket.on('player connect', function (play) {
+    //console.log("sending");
+    //console.log(players);
+    socket.emit('load game', { player: players });
+    id = play.id;
+    players.push(play);
+    io.emit('player connect', play);
+    console.log("Total Player :" + players.length);
+  });
 
-    socket.on('disconnect', function(){
-        console.log("Total Player :"+players.length);
+  socket.on('disconnect', function () {
+    for (var i = 0; i < players.length; i++)if (players[i].id == id) players.splice(i, 1);
+    console.log("Total Player :" + players.length);
 
-        for(var i=0;i<players.length;i++)if(players[i].id==id)players.splice(i, 1);
+    io.emit('player disconnect', id);
+  });
 
-        io.emit('player disconnect', id);
-    });
+  socket.on('player data', function (player) {
+    io.emit('player data', player);
+  });
 
-    socket.on('shared data', function(data){
-      io.emit('shared data', data);
-    });
+  socket.on('spawn bullet', function (bull) {
+    io.emit('spawn bullet', bull);
+  });
+
+  socket.on('hit bullet', function (bull) {
+    io.emit('hit bullet', bull);
+  });
+  socket.on('pass score', function (data) {
+    io.emit('pass score', data);
+  });
 });
 
-http.listen(3030, function(){
+http.listen(3030, function () {
   console.log('listening on *:3030');
 });
--- static/player.js
+++ static/player.js
@@ -1,70 +1,75 @@
-const imgSkin = {"9gag":"/image/9gag.png"};
+const imgSkin = { "9gag": "/image/9gag.png" };
+
 class Player {
-    constructor(name,xPos, yPos,id,skin) {
+    constructor(name, xPos, yPos, id, skin) {
         var _this = this;
-        this.name=name;
-        this.id= id;
-        this.size=60;
-        this.x=xPos;
-        this.y=yPos;
-        this.mX=this.x;
-        this.mY=this.y-1;
-        this.rad=0;
-        this.fx=0;
-        this.fy=0
-        this.skin=skin;
-        this.visible=false;
-        if(this.skin in imgSkin){
+        this.name = name;
+        this.id = id;
+        this.lvl = 0;
+        this.size = 60;
+        this.r = this.size*0.8;
+        this.x = xPos;
+        this.y = yPos;
+        this.mX = this.x;
+        this.mY = this.y - 1;
+        this.rad = 0;
+        this.fx = 0;
+        this.fy = 0
+        this.hp = 100;
+        this.skin = skin;
+        this.visible = false;
+
+        if (this.skin in imgSkin) {
             this.img = new Image();
-            this.imgLoad=false;
+            this.imgLoad = false;
             this.img.src = imgSkin[this.skin];
-            this.img.onload = function() {
-                _this.imgW = _this.size*5/4;
+            this.img.onload = function () {
+                _this.imgW = _this.size * 5 / 4;
                 _this.imgH = _this.imgW * _this.img.height / _this.img.width;
                 //console.log(this.name +" IMG loaded");
                 _this.imgLoad = true;
             };
         }
     }
-    push(fx,fy){
-        this.fx+=fx;
-        this.fy+=fy;
+    push(fx, fy) {
+        this.fx += fx;
+        this.fy += fy;
     }
-    update(){
-        this.x+=this.fx;
-        this.y+=this.fy;
-        this.fx=0;
-        this.fy=0;
+    update() {
+        this.x += this.fx;
+        this.y += this.fy;
+        this.fx = 0;
+        this.fy = 0;
 
         var deltaX = this.mX - this.x;
         var deltaY = this.mY - this.y;
-        this.rad = Math.atan2(deltaY, deltaX) +Math.PI/2;
+        this.rad = Math.atan2(deltaY, deltaX) + Math.PI / 2;
     }
     draw(canvas) {
-        if(!this.visible)return;
+        if (!this.visible) return;
         var ctx = canvas.getContext("2d");
 
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(this.rad);
 
-        if(this.skin in imgSkin){
-            if(this.imgLoad)ctx.drawImage(this.img,-this.imgW/2, -this.imgH/2,this.imgW,this.imgH);
+        if (this.skin in imgSkin) {
+            if (this.imgLoad) ctx.drawImage(this.img, -this.imgW / 2, -this.imgH / 2, this.imgW, this.imgH);
         }
-        else{
+        else {
             ctx.fillStyle = '#C0C0C0';
             ctx.beginPath();
-            ctx.moveTo(0, -this.size/2);
-            ctx.lineTo(0,this.size/3);
-            ctx.lineTo(0-this.size/1.8,this.size/1.5);
+            ctx.moveTo(0, -this.size / 2);
+            ctx.lineTo(0, this.size / 3);
+            ctx.lineTo(0 - this.size / 1.8, this.size / 1.5);
             ctx.closePath();
             ctx.fill();
 
             ctx.fillStyle = '#808080';
             ctx.beginPath();
-            ctx.moveTo(0, -this.size/2);
-            ctx.lineTo(0,this.size/3);
-            ctx.lineTo(this.size/1.8,this.size/1.5);
+            ctx.moveTo(0, -this.size / 2);
+            ctx.lineTo(0, this.size / 3);
+            ctx.lineTo(this.size / 1.8, this.size / 1.5);
             ctx.closePath();
             ctx.fill();
         }
@@ -77,16 +82,29 @@ class Player {
         ctx.stroke();*/
     }
     drawText(canvas) {
-        if(!this.visible)return;
+        if (!this.visible) return;
         var ctx = canvas.getContext("2d");
 
-        ctx.font = "bold 20px Arial";
-        canvas.style.letterSpacing = '4px';
+        var wp = this.hp*0.1, hi = 6, fs = 20;
+        ctx.fillStyle = '#DC143C';
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(this.x+wp, this.y + this.size+fs/3);
+        ctx.lineTo(this.x-wp, this.y + this.size+fs/3);
+        ctx.lineTo(this.x-wp, this.y + hi + this.size+fs/3);
+        ctx.lineTo(this.x+wp, this.y + hi + this.size+fs/3);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.font = "bold "+fs+"px Arial";
+        //canvas.style.letterSpacing = '2px';
         ctx.textAlign = "center";
         ctx.strokeStyle = 'black';
-        ctx.lineWidth = 8;
-        ctx.strokeText(this.name, this.x, this.y+this.size);
+        ctx.lineWidth = 4;
+        ctx.strokeText(this.name, this.x, this.y + this.size);
         ctx.fillStyle = 'white';
-        ctx.fillText(this.name, this.x, this.y+this.size);
+        ctx.fillText(this.name, this.x, this.y + this.size);
     }
 }
